86c9409eb6de41f0087d4282032c3363a464d90f
Ashutosh Kumar Roy <ashu00619@gmail.com>
2023-07-26 12:18:13 +0530

    Other pages urls are added

--- views.py
+++ views.py
@@ -6,3 +6,23 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def home():
     return render_template("home_page.html")
+
+
+@views.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@views.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+
+@views.route("/benchmarking")
+def benchmarking():
+    return render_template("benchmarking.html")
+
+
+@views.route("/workloads")
+def workload():
+    return render_template("workload.html")
